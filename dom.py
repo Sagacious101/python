b070ef3cab84931e07b7a2ea7d2fd14891b36f07
Sagacious101 <116090716+Sagacious101@users.noreply.github.com>
2022-10-25 16:51:50 +0300

    Add files via upload

--- dom.py
+++ dom.py
@@ -1,37 +1,97 @@
 import turtle as t
+import math
+
+t.shape("turtle")
+t.speed(0)
 
 
 
-def draw_house(x, y, walls_width, walls_height):
-    '''
-
-    x - лувфй нижний угол стены
-    y - лувый нижний угол стены
-    walls_width - ширина стен
-    walls_height -высота стен
-
-
-    '''
-    draw_walls(x, y, walls_width, walls_height)
-    draw_roof()
-    draw_door()
-
-
-def draw_walls(x, y, walls_width, walls_height):
-    print(f"нарисовал стену из {x}, {y}")
-    print(f"ширина стен {walls_width}")
-    print(f"высота стен {walls_height}")
-
-def draw_roof():
-    print("нарисовал крышу")
+"""
+walls_width = int(input("Введите ширину стен "))
+walls_height = int(input("Введите высоту стен "))
+roof_height = int(input("Введите сторону "))
+roof_side = math.sqrt((walls_width / 2) ** 2 + roof_height ** 2)
+roof_angle =  math.degrees(math.atan(roof_height / (walls_width/2)))
+walls_color = input("стены цвет ")
+roof_color = input('крыша цвет ')
+door_height = int(input('дверь высота '))
+door_width = int(input("дверь ширина "))
+door_color = input("цвет двери ")
+"""
 
 
-def draw_door():
-    print("нарисовал дверь")
+"""
+x - левый нижний угол стены
+y - левый нижний угол стены
+walls_height - высота стен
+walls_wight - ширина стен
+door_x - левый нижний угол стены
+door_y - левый нижний угол стены
+door_width - ширина двери
+door_height - высота
+"""
+
+def draw_walls(walls_x, walls_y, walls_height, walls_width, walls_color):
+    t.penup()
+    t.goto(walls_x,walls_y)
+    t.pendown()
+    t.fillcolor(walls_color)
+    t.begin_fill()
+    for i in range (2):
+        t.forward(walls_width)
+        t.left(90)
+        t.forward(walls_height)
+        t.left(90)
+    t.end_fill()
+
+
+def draw_roof(roof_height,walls_width,roof_color):
+    print("Нарисовали крышу")
+    t.fillcolor(roof_color)
+    roof_side = math.sqrt((walls_width / 2) ** 2 + roof_height ** 2)
+    roof_angle =  math.degrees(math.atan(roof_height / (walls_width/2)))
+    t.right(roof_side)
+    t.forward(100)
+
+
+def draw_doors(door_width,door_height,door_color,walls_x,walls_y,walls_width,walls_height):
+    print("Нарисовали двери")
+    door_x = (walls_x + (walls_width/2 - door_width/2))
+    door_y = walls_y
+    t.penup()
+    t.goto(door_x,walls_y)
+    t.pendown()
+    t.fillcolor(door_color)
+    t.begin_fill()
+    t.left(90)
+    t.forward(door_height)
+    t.right(90)
+    t.forward(door_width)
+    t.right(90)
+    t.forward(door_height)
+    t.right(90)
+    t.forward(door_width)
+    t.end_fill()
 
 
 
-draw_house(-100, -200)
-walls_width(200)
-walls_height(100)
+def draw_house(walls_x,
+walls_y,
+walls_width,
+walls_height,
+walls_color,
+door_width,
+door_height,
+door_color,
+roof_height,
+roof_color
+):
+    draw_walls(walls_x, walls_y, walls_height, walls_width, walls_color)
+    draw_roof(roof_height,roof_color)
+    draw_doors(door_width,door_height,door_color,walls_x,walls_y,walls_width,walls_height)
+
+
+draw_house(0, 0, 200, 100,"purple", 50, 70, "blue", 50, "purple")
+
+t.done()
 
